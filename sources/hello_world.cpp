4c756dc78fba4cede2d89a2f05ed6762117771fb
HECCYLLIujTbmy <sokolovskijvladislav23@gmail.com>
2023-04-16 01:39:25 -0400

    new hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-cout<<"TTpuBeT Mup";
+string name;
+cout<<"Say your name"<<endl;
+cin>>name;
+cout<<endl<<"TTpuBeT Mup OT - "<<name;
 }
